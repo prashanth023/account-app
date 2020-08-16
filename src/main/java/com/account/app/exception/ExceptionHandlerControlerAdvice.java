2b6197625c8a0200e896bc79ab0bae4283f5a0df
prashanth023 <m733953@gmail.com>
2020-08-16 13:09:57 +0530

    rest api exception added

--- src/main/java/com/account/app/exception/ExceptionHandlerControlerAdvice.java
+++ src/main/java/com/account/app/exception/ExceptionHandlerControlerAdvice.java
@@ -1,8 +1,14 @@
 package com.account.app.exception;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -62,5 +68,28 @@ public class ExceptionHandlerControlerAdvice {
 		error.setRequestedUri(request.getRequestURI());
 
 		return error;
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public @ResponseBody ExceptionResponse handleValidationExceptions(
+	  MethodArgumentNotValidException ex,final HttpServletRequest request) {
+		
+		ExceptionResponse error = new ExceptionResponse();
+		
+		error.setRequestedUri(request.getRequestURI());
+	   
+		List<ObjectError> errors=ex.getBindingResult().getAllErrors();
+		Iterator<ObjectError> it=errors.iterator();
+		String errMsg="";
+		while(it.hasNext()) {
+			
+		 ObjectError err=it.next();
+		String  errMs1g=((FieldError) err).getField()+" -"+err.getDefaultMessage();
+		 errMsg=errMs1g+",";
+		 
+		}
+		error.setErrorMessage(errMsg);
+	    return error;
 	}
 }
